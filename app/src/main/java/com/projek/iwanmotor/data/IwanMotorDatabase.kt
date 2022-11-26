625d5cb254b693f7d78acc860d49877bf422e46c
Nazirman <nzryanzr@gmail.com>
2022-11-26 18:47:33 +0700

    backend - adding database (changes databaseapplicaiton and IwanMotorDatabase.kt)

--- app/src/main/java/com/projek/iwanmotor/data/IwanMotorDatabase.kt
+++ app/src/main/java/com/projek/iwanmotor/data/IwanMotorDatabase.kt
@@ -4,29 +4,31 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.projek.iwanmotor.data.barang.Barang
+import com.projek.iwanmotor.data.barang.BarangDao
+import com.projek.iwanmotor.data.transaksi.Transaksi
+import com.projek.iwanmotor.data.transaksi.TransaksiDao
 import com.projek.iwanmotor.data.user.User
 import com.projek.iwanmotor.data.user.UserDao
 
-/**
- * Database class with a singleton INSTANCE object.
- */
-@Database(entities = [User::class], version = 1, exportSchema = false)
-abstract class RoomDatabase : RoomDatabase() {
-
-    abstract fun itemDao(): UserDao
+@Database(entities = [User::class, Barang::class, Transaksi::class], version = 1, exportSchema = false)
+abstract class IwanMotorDatabase : RoomDatabase() {
+    abstract fun barangDao(): BarangDao
+    abstract fun transaksiDao(): TransaksiDao
+    abstract fun userDao() : UserDao
 
     companion object {
         @Volatile
-        private var INSTANCE: RoomDatabase? = null
+        private var INSTANCE: IwanMotorDatabase? = null
 
-        fun getDatabase(context: Context): RoomDatabase {
+        fun getDatabase(context: Context): IwanMotorDatabase {
             // if the INSTANCE is not null, then return it,
             // if it is, then create the database
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
-                    RoomDatabase::class.java,
-                    "database"
+                    IwanMotorDatabase::class.java,
+                    "item_database"
                 )
                     // Wipes and rebuilds instead of migrating if no Migration object.
                     // Migration is not part of this codelab.
